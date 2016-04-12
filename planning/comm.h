5370310433aa9fb66a79735111fa7a6c6fc18619
jmb <juanmbellini@hotmail.com>
2016-04-12 18:31:12 -0300

    comm.h

--- planning/comm.h
+++ planning/comm.h
@@ -1,11 +1,29 @@
+typedef struct server_t* Server
 typedef struct connection_t * Connection
+typedef struct request_t * Request;
+typedef struct response_T * Response;
+typedef struct message_t * Message // Maybe we could use the same structre for request and response
 
 
-Connection openConnection();
 
+/*
+ * Creates a connection between a client and a server
+ */
+Connection openConnection(Server server);
+
+/*
+ * Closes a connection (i.e. finishes connection)
+ */
 void closeConnection(Connection connection);
 
+/*
+ * Makes a request through connection, sending a message
+ * Waits till the server responses
+ */
+Message request(Connection connection, Message message);
 
-
-void listenToRequest();
+/*
+ * Listens to request in a specific connection
+ */
+void listenToRequest(Connection connection);
 
