74e016e1b448bcca31bc0b47f12a0bc0509691eb
jmb <juanmbellini@hotmail.com>
2016-04-13 00:04:57 -0300

    sarasa

---
+++ planning/mainServerMain.c
@@ -0,0 +1,10 @@
+
+
+int main(void) {
+	
+	// Here goes a function to log that the service is running (communicate to logging server)
+	while (1) {
+		//Wait till client wants to connect
+	}
+	// Here goes a function to log that the service is down (communicate to logging server)
+}
