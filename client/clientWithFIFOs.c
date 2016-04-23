87db2a2a0803290c8158c83fce028d93e3135854
Juan Li Puma <juanlipuma94@gmail.com>
2016-04-23 11:51:00 -0300

    Reorganized code and simplified client code

--- client/clientWithFIFOs.c
+++ client/clientWithFIFOs.c
@@ -6,49 +6,22 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> //For sleep
+#include <unistd.h> //For sleeping
 #include <config.h>
 #include <comm.h>
-#include <database.h>
-#include <string.h>
 
 int main(int argc, char** argv) {
-    /*
-     * Connection test - when running, open a console and CD into /tmp
-     * When the FIFOs are created, run
-     * cat (pid)-out to read first message
-     * cat (pid)-out again to read CLOSE message
-     */
-    //    FILE *f = fopen(getServerAddress(), "w");
-    //    if(f == NULL) {
-    //        fprintf(stderr, "Couldn't open server FIFO.\n");
-    //        return -1;
-    //    }
-    //    char *str = "Hello!";
-    //    int len = strlen(str)+1;
-    //    int written = 0;
-    //    while(written < sizeof(len)) {
-    //        written += write(fileno(f), (&len)+written, sizeof(len)-written);
-    //    }
-    //    written = 0;
-    //    while(written < sizeof(len)) {
-    //        written += write(fileno(f), str+written, len-written);
-    //    }
-    //    printf("Successfully wrote to server.\n");
-    //    return 0;
-    
-    
-    printf("Connecting to server via %s\n", getServerAddress());
-    fflush(stdout);
+	printf("Connecting to server via %s...", getServerAddress());
+	fflush(stdout);
     Connection c = conn_open(getServerAddress());
+    printf("connected.\nSending random message...");
     fflush(stdout);
     conn_send(c, "Hello, server!", 15);
+    printf("sent.\nSleeping...");
+	fflush(stdout);
+    sleep(5);
+    printf("woke up.\nShutting down.\n");
+	fflush(stdout);
     conn_close(c);
-    fflush(stdout);
-    
-    //    sqlite3 *db = connectToDB();
-    //    printf("Successfully connected to database.\n");
-    //    printf("Disconnecting from DB returned %i\n", disconnectFromDB(db));
     return 0;
 }
--- common/lib.c
+++ common/lib.c
@@ -3,24 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * Gets the position of <i>needle</i> in <i>haystack</i>, if found.
- * 
- * @see http://stackoverflow.com/questions/2091825/strpos-in-c-how-does-it-work
- * @param haystack The string to search in.
- * @param needle The string to search for.
- * @return The starting position of <i>needle</i> in <i>haystack</i>, or -1 if
- * not found.
- */
-int indexOf(const char *haystack, const char *needle) {
-    char *p = strstr(haystack, needle);
-    if (p) {
-        return p - haystack;        
-    }
-    return -1;
-}
-
 int ensureRead(void *dest, size_t bytes, int fd) {
+	if(bytes == 0)
+		return 1;
+
 	int readBytes = 0, readResult;
 	while(readBytes < bytes) {
 		readResult = read(fd, (char *)dest+readBytes, bytes-readBytes);
@@ -33,11 +19,14 @@ int ensureRead(void *dest, size_t bytes, int fd) {
 }
 
 int ensureWrite(const void *src, size_t bytes, int fd) {
+	if(bytes == 0)
+		return 1;
+
 	int writtenBytes = 0, writeResult;
 	while(writtenBytes < bytes) {
 		writeResult = write(fd, (char*)src+writtenBytes, bytes-writtenBytes);
 		if(writeResult == -1) {
-			return -1;
+			return 0;
 		}
 		writtenBytes += writeResult;
 	}
