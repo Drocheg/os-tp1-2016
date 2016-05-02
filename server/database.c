310ffc68bd66209e427512fd5cf2c1848e9ec49d
Juan Li Puma <juanlipuma94@gmail.com>
2016-05-02 20:35:32 -0300

    Added function to query available products
    A layer of abstraction from SQL allows any server to get
    currently available products

--- server/database.c
+++ server/database.c
@@ -1,8 +1,36 @@
 #include "database.h"
+#include <product.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+static int rowCounter(int *result, int numCols, char **colData, char **colName) {
+    *result = strtol(colData[0], NULL, 10);
+    return 0;
+}
+
+static int productLoader(Product *array, int numCols, char **colData, char **colName) {
+    int i = 0;
+    //TODO don't go to end of array on every item
+    while(array[i] != NULL) //Append to end of array
+        i++;
+    array[i] = newProduct(colData[0], colData[1], atof(colData[2]), atoi(colData[3]));
+    return 0;
+}
 
 int db_run_query(sqlite3* connection, const char* query, databaseCallback callback, void* firstCallbackArgument, char **errorMsgDest) {
     return sqlite3_exec(connection, query, callback, firstCallbackArgument, errorMsgDest);
+}
+
+int db_get_products(sqlite3* connection, Product **destArray) {
+    char *errormsg;
+    int numProducts;
+    db_run_query(connection, "SELECT count(id) FROM products", (databaseCallback)rowCounter, &numProducts, &errormsg);
+    if(numProducts <= 0) {
+        return -1;
+    }
+    *destArray = calloc(numProducts, sizeof(*destArray));
+    db_run_query(connection, "SELECT name, description, price, quantity FROM products", (databaseCallback)productLoader, *destArray, &errormsg);
+    return numProducts;
 }
 
 sqlite3 *db_connect() {
--- server/database.h
+++ server/database.h
@@ -9,13 +9,48 @@
 #define DATABASE_H
 
 #include "../SQLite/src/sqlite3.h"
+#include <product.h>
 
 typedef int (*databaseCallback)(void*, int, char**, char**);
 
+/**
+ * Opens a connection to the database.
+ * 
+ * @return sqlite3* The SQLite3 connection.
+ */
 sqlite3 *db_connect();
 
+/**
+ * Runs a custom query on the database.
+ * 
+ * @param connection A connection to the database.
+ * @param query The query to run.
+ * @param callback A callback function to run on every retrieved row.
+ * @param firstCallbackArgument The first argument to pass to the callback
+ * function.
+ * @param errorMsgDest Where to store any error messages. <b>NOTE: </b> If an
+ * error does occur, call <i>sqlite3_free(errorMsgDest)</i> to free resources.
+ * @return int A success or error code, according to <i>sqlite3_exec</i>
+ * @see sqlite3_exec
+ */
 int db_run_query(sqlite3* connection, const char* query, databaseCallback callback, void* firstCallbackArgument, char **errorMsgDest);
 
+/**
+ * Queries the database and returns all products, including those with no stock.
+ * 
+ * @param connection A database connection through which to get data.
+ * @param destArray Where to store the retrieved products.
+ * @return int The number of retrieved products.
+ */
+int db_get_products(sqlite3* connection, Product **destArray);
+
+/**
+ * Closes a connection to the database.
+ * 
+ * @param connection The connection to close and free resources for.
+ * @return SQLITE_OK on success, or SQLITE_BUSY if the connection is handling
+ * other requests and wasn't closed.
+ */
 int db_disconnect(sqlite3 *connection);
 
 #endif /* DATABASE_H */
