295c5f1b29ebdca1a53477195dc1bd49e734c371
Diego Maria De Grimaudet De Rochebouet <dgrimau@localhost.localdomain>
2016-05-05 17:50:57 -0300

    Serializable Order

--- common/order.c
+++ common/order.c
@@ -6,9 +6,9 @@ struct orderEntry_t{
 };
 
 struct order_t {
-	struct orderEntry_t items[16]; //Se podria hacer dinamico
-	char * address;
+	char * address; //Cuando malloc? Supongo que en el momento de agregarle el address.
 	int numEntries; 
+	struct orderEntry_t items[16]; //Se podria hacer dinamico	
 };
 
 
@@ -42,6 +42,38 @@ int addToOrder(Order order, int product_id, int quantity){
 		order->items[i].quantity  = quantity;
 	}
 	return 0;
+}
+
+
+int serializeOrder(const Order o, void **dest) {
+    //TODO check for errors and return NULL
+    int offset = 0;
+    int addressLen = strlen(o->address)+1;
+    int totalLen = sizeof(addressLen) + addressLen + sizeof(o->numEntries) + sizeof(o->items);
+    *dest = malloc(totalLen);
+    memcpy(*dest, &addressLen, sizeof(addressLen));
+    offset += sizeof(addressLen);
+    memcpy(*dest+offset, o->address, addressLen);
+    offset += addressLen;
+    memcpy(*dest+offset, &(o->numEntries), sizeof(o->numEntries));
+    offset += sizeof(o->numEntries);
+    memcpy(*dest+offset, &(o->items), sizeof(o->items));
+    return 1;
+}
+
+Product unserializeOrder(const void* data) {
+    //TODO check for errors and return NULL
+    Order order = malloc(sizeof(*order));
+    int addressLen, offset = 0;
+    memcpy(&addressLen, data, sizeof(addressLen));
+    offset += sizeof(addressLen);
+    order->address = malloc(addressLen);
+    strcpy(order->address, data+offset);
+    offset += addressLen;
+    memcpy(&(order->numEntries), data+offset, sizeof(order->numEntries));
+    offset += sizeof(order->numEntries);
+    memcpy(&(order->items), data+offset, sizeof(order->items));
+    return result;
 }
 
 void printTemporalOrder(Order order){ //TODO Borrar o hacer bien
