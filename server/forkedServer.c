54a9bc76cbba8c6663e28d067c33b664ee536035
Diego Maria De Grimaudet De Rochebouet <dgrimau@localhost.localdomain>
2016-05-10 12:53:24 -0300

    Casi terminado forkedServer, falta contacto con base de Datos

--- server/forkedServer.c
+++ server/forkedServer.c
@@ -6,10 +6,12 @@
 
 #include "product.h"
 
+void forkedServer(Connection c, Connection dbConnection);
+void sendProducts(Connection c, Connection dbConnection);
+void receiveOrder(Connection c, Connection dbConnection);
+void endConnection(Connection c, Connection dbConnection);
 
-
-
-void forkedServer(Connection c){ //RequestProd 1 y  finish 2
+void forkedServer(Connection c, Connection dbConnection){ //RequestProd 1 y  finish 2
 
 	//De donde saco la base de datos?
 	int done = 0;
@@ -22,17 +24,65 @@ void forkedServer(Connection c){ //RequestProd 1 y  finish 2
     	int msgCode = *((int*)clienteData);
     	switch(msgCode){//Rodrigo prefiere siempre cosas bien, paja
     		case 1:
-    			sendProducts(c);
+    			sendProducts(c, dbConnection);
     			break;
     		case 2:	
-    			receiveOrder(c);
+    			receiveOrder(c, dbConnection);
     			break;
     		case 3:
-    			endConnection(c);
+    			endConnection(c, dbConnection);
     			break;
     		default:
     			printf("WAT\n");
     			break;
     	}   
 	}while(!done);
+}
+
+
+void sendProducts(Connection c, Connection dbConnection){
+
+    Product * products;
+    products = getProdcuts(dbConnection) //TODO pedir productos a la base de datos
+
+    int responseCode = 1;
+    conn_send(c, &responseCode, sizeof(responseCode));
+
+
+    int numProducts = getNumProducts(dbConnection)  //TODO pedir la cantidad de productos a la base de datos (Que sea correspondiente a los productos, sin cambios en el medio)
+    conn_send(c, &numProducts,sizeof(numProducts));
+
+
+    void * serializedProduct;
+    int serializedProductSize;
+
+    for(int i = 0; i<*numProducts; i++){
+        serializedProductSize=serializeProduct(products[i], serializedProduct);
+        conn_send(c,serializedProduct,serializedProductSize);
+    } 
+
+}
+
+void receiveOrder(Connection c, Connection dbConnection){
+
+    void * serializedOrder;
+    int serializedOrderSize;
+    conn_receive(c, serializedOrder, &serializedOrderSize);
+    Order order = unserializeOrder(serializedOrder);
+
+    int stockCheck = checkStockAndChange(order,dbConnection);//Se tiene que tener cuidado de que nadie aceda a la base de datos entre el check y el change
+    if(stockCheck<0){ 
+        printf("No hay stock\n");
+        return; 
+    }
+    printf("Compra satisfactoria");
+
+    int responseCode = 1;
+    conn_send(c, &responseCode, sizeof(responseCode));
+
+}
+
+
+void endConnection(Connection c, Connection dbConnection){
+    //TODO
 }
