1a35060d0b92d510713e3d8ab35e5310195e57d0
jmb <juanmbellini@hotmail.com>
2016-05-13 17:51:10 -0300

    Finish with Sockets. Please, test with server and client, and give me feedback.

--- common/commWithSockets.c
+++ common/commWithSockets.c
@@ -1,9 +1,26 @@
 #include "comm.h"
+#include "lib.h"
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <regex.h> 
+#include <stdio.h>
+
+
 
 
 struct connection_t {
 	char *ip;
+	in_port_t port;
+	int socketfd;
+
+};
+
+
+struct conn_params_t {
 	in_port_t port;
 	int socketfd;
 
@@ -25,8 +42,10 @@ static char* getPortRegEx();
 static int checkIP(const char *ip);
 static int checkPort(const char *portStr);
 static int isValidAddress(const char *address);
-static int initialize(Connection connection, const char *address);
-static int createTCPSocket(Connection connection);
+static int initializeClient(Connection connection, const char *address);
+static int initializeServer(ConnectionParams connectionParams, char *listeningAddress);
+static int createTCPSocketForClient(Connection connection);
+static int createTCPSocketForServer(ConnectionParams connection);
 
 
 
@@ -56,7 +75,7 @@ Connection conn_open(const char* address) {
 		return NULL;
 	}
 
-	return (initialize(connection, address) == -1) ? NULL : connection;
+	return (initializeClient(connection, address) == -1) ? NULL : connection;
 
 }
 
@@ -69,8 +88,15 @@ Connection conn_open(const char* address) {
  */
 int conn_close(Connection connection) {
 
-
-
+	if (!conn_send(connection, MESSAGE_CLOSE, sizeof(MESSAGE_CLOSE))) {
+		return 0; /* Couldn't communicate with server to close */
+	}
+	if (shutdown_wrp(connection->socketfd, SHUT_RDWR)) {
+   		return 0; /* Couldn't close connection */
+	}
+	free(connection->ip);
+	free(connection);
+	return 1;
 
 }
 
@@ -87,9 +113,10 @@ int conn_close(Connection connection) {
  */
 int conn_send(const Connection connection, const void* data, const size_t length) {
 
-
-
-
+	if(length == 0) {
+		return 1;
+	}
+	return ensureWrite(&length, sizeof(length), connection->socketfd) && ensureWrite(data, length, connection->socketfd);
 }
 
 /*
@@ -106,9 +133,12 @@ int conn_send(const Connection connection, const void* data, const size_t length
  */
 int conn_receive(const Connection conn, void** data, size_t* length) {
 
-
-
-
+	int result1, result2;
+	if (!ensureRead(length, sizeof(*length), conn->socketfd)) {
+		return 0;
+	}
+	*data = malloc(*length);
+	return ensureRead(*data, *length, conn->socketfd);
 }
 
 /**
@@ -123,10 +153,17 @@ int conn_receive(const Connection conn, void** data, size_t* length) {
 */
 ConnectionParams conn_listen(char *listeningAddress) {
 
+	ConnectionParams connectionParams;
 
+	if(!checkPort(listeningAddress)) {
+		return NULL;
+	}
 
-
-
+	connectionParams = malloc(sizeof(*connectionParams));
+	if (connectionParams == NULL) {
+		return NULL;
+	}
+	return (initializeServer(connectionParams, listeningAddress) == -1) ? NULL : connectionParams;
 }
 
 /**
@@ -139,11 +176,21 @@ ConnectionParams conn_listen(char *listeningAddress) {
 */
 Connection conn_accept(ConnectionParams params) {
 
+	int newFD = accept(params->socketfd, NULL, NULL);
+	Connection connection;
+	if (newFD < 0) {
+		return NULL; /* Couldn't create a new FD for the connection */
+	}
+	connection = malloc(sizeof(*connection));
+	if (connection == NULL) {
+		return NULL; /* Couldn't allocate memory for the connection struct */
+	}
 
+	connection->ip = NULL;
+	connection->port = params->port;
+	connection->socketfd = newFD;
 
-
-
-
+	return connection;
 }
 
 
@@ -203,7 +250,7 @@ static char* getPortRegEx() {
 static int checkIP(const char *ip) {
 
 	regex_t regex;
-	char *expr = getAddressRegEx();
+	char *expr = getIPRegEx();
 	int result = 0;
 
 	if (expr == NULL) {
@@ -212,7 +259,7 @@ static int checkIP(const char *ip) {
 
 	regcomp(&regex, expr, REG_EXTENDED);
 	result = !regexec(&regex, ip, 0, NULL, 0); /*Returns 0 if success, or a non-zero code if not match */
-	free(expr); /* Must free because getAddressRegex function allocates memory */
+	free(expr); /* Must free because getIpRegex function allocates memory */
 	return result;
 
 }
@@ -260,7 +307,7 @@ static int isValidAddress(const char *address) {
 
 	ipPart = calloc(1, (colonPosition + 1) * sizeof(char));
 	portPart = calloc(1, (portPartLength + 1) * sizeof(char));
-	memcpy(ipPart, address, colonPosition * sizeeof(char));
+	memcpy(ipPart, address, colonPosition * sizeof(char));
 	memcpy(portPart, address + colonPosition + 1, portPartLength * sizeof(char));
 
 	flag1 = checkIP(ipPart);
@@ -274,9 +321,10 @@ static int isValidAddress(const char *address) {
 }
 
 /*
- * Creates the TCP Socket
+ * Creates the TCP Socket for a client connection
+ * Returns 0 on success, -1 otherwise
  */
-static int createTCPSocket(Connection connection) {
+static int createTCPSocketForClient(Connection connection) {
 
 	struct sockaddr_in serverAddress;
 
@@ -296,29 +344,64 @@ static int createTCPSocket(Connection connection) {
 }
 
 /*
- * Initializes a connections
+ * Initializes a client connection
  * Returns 0 on sucess, or -1 otherwise
  */
-static int initialize(Connection connection, const char *address) {
+static int initializeClient(Connection connection, const char *address) {
 
-	int colonPosition = searchForColon();
+	int colonPosition = searchForColon(address);
 	connection->ip = calloc(1, (colonPosition + 1) * sizeof(char));
 
 	if (connection->ip == NULL) {
 		return -1;
 	}
 	memcpy(connection->ip, address, colonPosition * sizeof(char));
-	connection->port = (in_port_t)atoi((char *)(((void *)address) + colonPosition + 1))
+	connection->port = (in_port_t)atoi((char *)(((void *)address) + colonPosition + 1));
 
-	return createTCPSocket(connection);
+	return createTCPSocketForClient(connection);
 
 }
 
+/*
+ * Creates the TCP Socket for a server connection
+ * Returns 0 on success, -1 otherwise
+ */
+static int createTCPSocketForServer(ConnectionParams connectionParams) {
+
+	struct sockaddr_in mainServer;
+
+	mainServer.sin_family = AF_INET;
+    mainServer.sin_addr.s_addr = htonl(INADDR_ANY); /* Enables connection through all present interfaces */
+    mainServer.sin_port = htons(connectionParams->port);
+
+    if ((connectionParams->socketfd = socket_wrp(PF_INET, SOCK_STREAM, 0)) < 0) {
+    	return -1;
+	}
+	if (bind_wrp(connectionParams->socketfd, (struct sockaddr*)&mainServer, sizeof(mainServer))) {
+    	return -1;
+    }
+    if (listen_wrp(connectionParams->socketfd, 10)) {
+   		return -1;
+   	}
+
+   	return 0;
+}
+
+/*
+ * Initializes a server connection
+ * Returns 0 on sucess, or -1 otherwise
+ */
+static int initializeServer(ConnectionParams connectionParams, char *listeningAddress) {
+
+	connectionParams->port = atoi(listeningAddress);
+   	return createTCPSocketForServer(connectionParams);
+}
+
+
 
 /*********************************/
 /* Connecting functions wrappers */
 /*********************************/
-/* Connecting functions wrappers */
 static int inet_pton_wrp(int af, const char* src, void* dst) {
 
 	printf("%s\n", src);
