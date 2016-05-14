6dc332cf2c6516dd83e8f1e8bc149a6d42d010e1
Juan Li Puma <juanlipuma94@gmail.com>
2016-05-14 13:03:07 -0300

    Order changes
    -Documented header functions
    -FIXED: Un/serializing an order with an unspecified address could cause
    segmentation faults. Address now initialized to NULL and checked when un/
    serializing.
    -Added function to calculate order total
    -TODO: Include price in each order entry
    -FIXED: Some memcpy's were backwards. WARN: There might be more, I haven't
    thoroughly checked all of them.

--- common/order.c
+++ common/order.c
@@ -21,17 +21,22 @@ OrderEntry order_get_entry(Order o, int entryNum) {
     return o->items[entryNum];
 }
 
-int orderentry_id(OrderEntry e) {
+int orderentry_get_id(OrderEntry e) {
     return e->product_id;
 }
 
-int orderentry_quantity(OrderEntry e) {
+void orderentry_set_quantity(OrderEntry e, int newQuantity) {
+    e->quantity = newQuantity;
+}
+
+int orderentry_get_quantity(OrderEntry e) {
     return e->quantity;
 }
 
 Order order_new() {
     Order order = malloc(sizeof (*order));
     order->numEntries = 0;
+    order->address = NULL;
     return order;
 }
 
@@ -40,6 +45,18 @@ void order_free(Order o) {
         free(o->items[i]);
     }
     free(o);
+}
+
+float order_get_total(Order order) {
+    float result = 0;
+    for (int i = 0; i < order->numEntries; i++) {
+        result += 1/*ACA PRECIO*/ * order->items[i]->quantity;   //Aca Diego pone el precio de cada item
+    }
+    return result;
+}
+
+char* order_get_addr(Order order) {
+    return order->address;
 }
 
 void order_set_addr(Order order, char * address) {
@@ -62,8 +79,8 @@ int order_add(Order order, int product_id, int quantity) {
     } else if (i < ORDER_MAX_SIZE) {
         order->numEntries++;
         order->items[i] = malloc(sizeof (*(order->items[i])));
-        order->items[i].product_id = product_id;
-        order->items[i].quantity = quantity;
+        order->items[i]->product_id = product_id;
+        order->items[i]->quantity = quantity;
     } else {
         return -1;
     }
@@ -73,12 +90,14 @@ int order_add(Order order, int product_id, int quantity) {
 size_t order_serialize(const Order o, void **dest) {
     //TODO check for errors and return NULL
     int offset = 0;
-    size_t addressLen = strlen(o->address) + 1;
+    size_t addressLen = o->address == NULL ? 0 : strlen(o->address) + 1;
     size_t totalLen = sizeof (addressLen) + addressLen + sizeof (o->numEntries) + (sizeof (o->items) * sizeof (*(o->items[0])));    //TODO verify sum is correct
     *dest = malloc(totalLen);
     memcpy(*dest, &addressLen, sizeof (addressLen));
     offset += sizeof (addressLen);
-    memcpy(*dest + offset, o->address, addressLen);
+    if(addressLen > 0) {
+        memcpy(*dest + offset, o->address, addressLen);
+    }
     offset += addressLen;
     memcpy(*dest + offset, &(o->numEntries), sizeof (o->numEntries));
     offset += sizeof (o->numEntries);
@@ -98,16 +117,21 @@ Order order_unserialize(const void* data) {
     int offset = 0;
     memcpy(&addressLen, data, sizeof (addressLen));
     offset += sizeof (addressLen);
-    result->address = malloc(addressLen);
-    strncpy(result->address, data + offset, addressLen);
+    if(addressLen == 0) {
+        result->address = NULL;
+    }
+    else {
+        result->address = malloc(addressLen);
+        strncpy(result->address, data + offset, addressLen);
+    }
     offset += addressLen;
     memcpy(&(result->numEntries), data + offset, sizeof (result->numEntries));
     offset += sizeof (result->numEntries);
     for(int i = 0; i < result->numEntries; i++) {
         result->items[i] = malloc(sizeof(*(result->items[i])));
-        memcpy(data+offset, &(result->items[i]->product_id), sizeof(result->items[i]->product_id));
+        memcpy(&(result->items[i]->product_id), data+offset, sizeof(result->items[i]->product_id));
         offset += sizeof(result->items[i]->product_id);
-        memcpy(data+offset, &(result->items[i]->quantity), sizeof(result->items[i]->quantity));
+        memcpy(&(result->items[i]->quantity), data+offset, sizeof(result->items[i]->quantity));
         offset += sizeof(result->items[i]->quantity);
     }
     return result;
